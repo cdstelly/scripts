c56cf9c13408ccafea300f4f0eb5b7f18d0b6f49
cdstelly <cdstelly@gmail.com>
2017-01-11 18:57:18 -0600

    Added first radio - fm

---
+++ radio/fm_receiver.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python
+##################################################
+# Gnuradio Python Flow Graph
+# Title: Fm Receiver
+# Generated: Tue Nov 22 12:56:36 2016
+##################################################
+
+from gnuradio import analog
+from gnuradio import audio
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
+from gnuradio.filter import firdes
+from gnuradio.wxgui import fftsink2
+from gnuradio.wxgui import forms
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import osmosdr
+import time
+import wx
+
+class fm_receiver(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Fm Receiver")
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 8e6
+        self.frequency = frequency = 981e5
+        self.filter_width = filter_width = 200e3
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.notebook_0 = self.notebook_0 = wx.Notebook(self.GetWin(), style=wx.NB_TOP)
+        self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "Raw")
+        self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "After Demoduation")
+        self.Add(self.notebook_0)
+        _frequency_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._frequency_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_frequency_sizer,
+        	value=self.frequency,
+        	callback=self.set_frequency,
+        	label='frequency',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._frequency_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_frequency_sizer,
+        	value=self.frequency,
+        	callback=self.set_frequency,
+        	minimum=90e6,
+        	maximum=108e6,
+        	num_steps=100,
+        	style=wx.SL_VERTICAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_frequency_sizer)
+        _filter_width_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._filter_width_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_filter_width_sizer,
+        	value=self.filter_width,
+        	callback=self.set_filter_width,
+        	label='filter_width',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._filter_width_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_filter_width_sizer,
+        	value=self.filter_width,
+        	callback=self.set_filter_width,
+        	minimum=50e3,
+        	maximum=400e3,
+        	num_steps=100,
+        	style=wx.SL_VERTICAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_filter_width_sizer)
+        self.wxgui_fftsink2_1 = fftsink2.fft_sink_f(
+        	self.notebook_0.GetPage(1).GetWin(),
+        	baseband_freq=0,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=250e3,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="FFT Plot",
+        	peak_hold=False,
+        )
+        self.notebook_0.GetPage(1).Add(self.wxgui_fftsink2_1.win)
+        self.wxgui_fftsink2_0 = fftsink2.fft_sink_c(
+        	self.notebook_0.GetPage(0).GetWin(),
+        	baseband_freq=1015e5,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=500,
+        	average=False,
+        	avg_alpha=None,
+        	title="FFT Plot",
+        	peak_hold=False,
+        )
+        self.notebook_0.GetPage(0).Add(self.wxgui_fftsink2_0.win)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
+                interpolation=48,
+                decimation=400,
+                taps=None,
+                fractional_bw=None,
+        )
+        self.osmosdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + "" )
+        self.osmosdr_source_0.set_sample_rate(samp_rate)
+        self.osmosdr_source_0.set_center_freq(frequency, 0)
+        self.osmosdr_source_0.set_freq_corr(0, 0)
+        self.osmosdr_source_0.set_dc_offset_mode(0, 0)
+        self.osmosdr_source_0.set_iq_balance_mode(0, 0)
+        self.osmosdr_source_0.set_gain_mode(False, 0)
+        self.osmosdr_source_0.set_gain(20, 0)
+        self.osmosdr_source_0.set_if_gain(20, 0)
+        self.osmosdr_source_0.set_bb_gain(20, 0)
+        self.osmosdr_source_0.set_antenna("", 0)
+        self.osmosdr_source_0.set_bandwidth(0, 0)
+          
+        self.low_pass_filter_0 = filter.fir_filter_ccf(20, firdes.low_pass(
+        	1, samp_rate, filter_width, 10e3, firdes.WIN_HAMMING, 6.76))
+        self.blocks_wavfile_sink_0 = blocks.wavfile_sink("/Users/myla/SDR/FM Reciever/fm2.wav", 1, 48000, 8)
+        self.audio_sink_0 = audio.sink(48000, "", True)
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=250e3,
+        	audio_decimation=1,
+        )
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_0, 0))    
+        self.connect((self.analog_wfm_rcv_0, 0), (self.wxgui_fftsink2_1, 0))    
+        self.connect((self.low_pass_filter_0, 0), (self.analog_wfm_rcv_0, 0))    
+        self.connect((self.low_pass_filter_0, 0), (self.wxgui_fftsink2_0, 0))    
+        self.connect((self.osmosdr_source_0, 0), (self.low_pass_filter_0, 0))    
+        self.connect((self.rational_resampler_xxx_0, 0), (self.audio_sink_0, 0))    
+        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_wavfile_sink_0, 0))    
+
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.filter_width, 10e3, firdes.WIN_HAMMING, 6.76))
+        self.osmosdr_source_0.set_sample_rate(self.samp_rate)
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
+
+    def get_frequency(self):
+        return self.frequency
+
+    def set_frequency(self, frequency):
+        self.frequency = frequency
+        self.osmosdr_source_0.set_center_freq(self.frequency, 0)
+        self._frequency_slider.set_value(self.frequency)
+        self._frequency_text_box.set_value(self.frequency)
+
+    def get_filter_width(self):
+        return self.filter_width
+
+    def set_filter_width(self, filter_width):
+        self.filter_width = filter_width
+        self._filter_width_slider.set_value(self.filter_width)
+        self._filter_width_text_box.set_value(self.filter_width)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.filter_width, 10e3, firdes.WIN_HAMMING, 6.76))
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    (options, args) = parser.parse_args()
+    tb = fm_receiver()
+    tb.Start(True)
+    tb.Wait()
