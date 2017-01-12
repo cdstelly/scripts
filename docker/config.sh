fbff33507713b8586a029f4ab27d48dfd1c2caaf
Christopher Stelly <cdstelly@gmail.com>
2017-01-12 16:45:24 -0600

    Update config.sh

--- docker/config.sh
+++ docker/config.sh
@@ -2,5 +2,12 @@ $ sudo vim /etc/sysctl.conf
 
 # append
 vm.max_map_count=262144
-
 $ sudo sysctl -p 
+
+
+
+
+#change storage dir of containers
+$ sudo vim /etc/default/docker
+DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4 -g /mnt/containers"
+$ sudo systemctl restart docker
