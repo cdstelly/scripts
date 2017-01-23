0053068dd41ea3270567e10bc85ea3bf298790f2
Christopher Stelly <cdstelly@gmail.com>
2017-01-22 20:37:25 -0600

    Update UpdateGopathHere.sh

--- go/UpdateGopathHere.sh
+++ go/UpdateGopathHere.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # Change do directory you want to work with first..
-export GOPATH=`pwd`
-export GOBIN=`pwd`/bin
+
+function ugph () {
+    export GOPATH=`pwd`
+    export GOBIN=`pwd`/bin
+    echo $GOPATH
+    echo $GOBIN
+}
