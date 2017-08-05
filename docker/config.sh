1573fcaec6658b43398cdaa17e32828c2adc0573
Christopher Stelly <cdstelly@gmail.com>
2017-08-05 01:05:25 -0500

    Update config.sh
    don't need all that whitespace!

--- docker/config.sh
+++ docker/config.sh
@@ -4,15 +4,10 @@ $ sudo vim /etc/sysctl.conf
 vm.max_map_count=262144
 $ sudo sysctl -p 
 
-
-
-
 #change storage dir of containers
 $ sudo vim /etc/default/docker
 DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4 -g /mnt/containers"
 $ sudo systemctl restart docker
-
-
 
 # bash exec shortcut
 function e()  {
